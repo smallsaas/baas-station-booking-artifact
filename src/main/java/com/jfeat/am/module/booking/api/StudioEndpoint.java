a39b21ee5cd0a5522e442b2eab0f55a84da9ed34
J4cob <asdeit@outlook.com>
2017-09-20 10:26:46 +0800

    add permission

--- src/main/java/com/jfeat/am/module/booking/api/StudioEndpoint.java
+++ src/main/java/com/jfeat/am/module/booking/api/StudioEndpoint.java
@@ -80,15 +80,14 @@ public class StudioEndpoint extends BaseController {
     *   CRUD about Studio
     * */
     @PostMapping
-    //@Permission(AdminPermission.CREATE)
-    //TODO 测试先不是使用权限。
+    @Permission(AdminPermission.CREATE)
     public Tip createStudio(@Valid @RequestBody Studio studio) {
         Integer result = sDservice.createMaster(studio);
         return SuccessTip.create(result);
     }
 
     @PutMapping
-    //@Permission(AdminPermission.EDIT)
+    @Permission(AdminPermission.EDIT)
     public Tip updateStudio(@Valid @RequestBody Studio studio) {
         Integer result = sDservice.updateMaster(studio);
         return SuccessTip.create(result);
@@ -101,7 +100,7 @@ public class StudioEndpoint extends BaseController {
     }
 
     @DeleteMapping("/{id}")
-   // @Permission(AdminPermission.DELETE)
+    @Permission(AdminPermission.DELETE)
     public Tip deleteStudio(@PathVariable long id) {
         Integer result = sDservice.deleteMaster(id);
         return SuccessTip.create(result);
--- src/main/resources/sql/sbseed-data.sql
+++ src/main/resources/sql/sbseed-data.sql
@@ -1,2 +1,8 @@
-
 SET FOREIGN_KEY_CHECKS=0;
+INSERT INTO `perm_group` (`id`, `name`, `identifier`) VALUES
+('909719474314252289', '纹绣快约', 'studio.management');
+
+INSERT INTO `perm` (`id`, `groupid`, `name`, `identifier`) VALUES
+('909719474314252290', '909719474314252289', '添加店铺', 'studio.create'),
+('909719474314252291', '909719474314252289', '修改店铺', 'studio.edit'),
+('909719474314252292', '909719474314252289', '删除店铺', 'studio.delete');
