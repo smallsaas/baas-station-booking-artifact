a62e61464a4dc3abbe760d8c3c8f67204c46e551
J4cob <asdeit@outlook.com>
2017-09-26 18:34:18 +0800

    config

---
+++ src/main/java/com/jfeat/am/module/booking/services/service/wechat/WechatConfigService.java
@@ -0,0 +1,9 @@
+package com.jfeat.am.module.booking.services.service.wechat;
+
+/**
+ * Created by Administrator on 2017/9/26.
+ */
+public interface WechatConfigService {
+
+
+}
---
+++ src/main/java/com/jfeat/am/module/booking/services/service/wechat/impl/WechatConfigServiceImpl.java
@@ -0,0 +1,16 @@
+package com.jfeat.am.module.booking.services.service.wechat.impl;
+
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.jfeat.am.module.booking.services.service.wechat.WechatConfigService;
+import com.jfinal.weixin.sdk.api.ApiConfig;
+import com.jfinal.weixin.sdk.api.ApiConfigKit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2017/9/26.
+ */
+@Service
+public class WechatConfigServiceImpl implements WechatConfigService {
+
+}
