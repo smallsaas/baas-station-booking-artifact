ee0e02aaa3ea4d79634f2deb2040153739b328f6
J4cob <asdeit@outlook.com>
2017-09-29 14:18:43 +0800

    提交缺失文件

---
+++ src/main/java/com/jfeat/am/module/booking/services/persistence/mapper/StudioServiceMapper.java
@@ -0,0 +1,16 @@
+package com.jfeat.am.module.booking.services.persistence.mapper;
+
+import com.jfeat.am.module.booking.services.persistence.model.StudioService;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+  *  Mapper 接口
+ * </p>
+ *
+ * @author J4cob
+ * @since 2017-09-28
+ */
+public interface StudioServiceMapper extends BaseMapper<StudioService> {
+
+}
---
+++ src/main/java/com/jfeat/am/module/booking/services/persistence/model/StudioService.java
@@ -0,0 +1,90 @@
+package com.jfeat.am.module.booking.services.persistence.model;
+
+import java.io.Serializable;
+
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableName;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author J4cob
+ * @since 2017-09-28
+ */
+@TableName("emb_studio_service")
+public class StudioService extends Model<StudioService> {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键
+     */
+	private Long id;
+    /**
+     * 服务类型
+     */
+	@TableField("type_id")
+	private Long typeId;
+    /**
+     * 技师ID
+     */
+	@TableField("doctor_id")
+	private Long doctorId;
+    /**
+     * 工作室ID
+     */
+	@TableField("studio_id")
+	private Long studioId;
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getTypeId() {
+		return typeId;
+	}
+
+	public void setTypeId(Long typeId) {
+		this.typeId = typeId;
+	}
+
+	public Long getDoctorId() {
+		return doctorId;
+	}
+
+	public void setDoctorId(Long doctorId) {
+		this.doctorId = doctorId;
+	}
+
+	public Long getStudioId() {
+		return studioId;
+	}
+
+	public void setStudioId(Long studioId) {
+		this.studioId = studioId;
+	}
+
+	@Override
+	protected Serializable pkVal() {
+		return this.id;
+	}
+
+	@Override
+	public String toString() {
+		return "StudioService{" +
+			"id=" + id +
+			", typeId=" + typeId +
+			", doctorId=" + doctorId +
+			", studioId=" + studioId +
+			"}";
+	}
+}
