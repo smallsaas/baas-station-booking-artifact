417a742d618693fbd5076e266a8a0dcdcecf9dd0
J4cob <asdeit@outlook.com>
2017-11-14 16:31:42 +0800

    GPS

--- src/main/java/com/jfeat/am/module/booking/api/StudioEndpoint.java
+++ src/main/java/com/jfeat/am/module/booking/api/StudioEndpoint.java
@@ -172,13 +172,13 @@ public class StudioEndpoint extends BaseController {
         BigDecimal latitude = new BigDecimal(lat);
         BigDecimal longitude = new BigDecimal(lng);
 
-        Customer customer = pathService.queryCustomerByUserId(userId);
+/*        Customer customer = pathService.queryCustomerByUserId(userId);
         if (customer != null) {
             customer.setLatitude(latitude);
             customer.setLongitude(longitude);
             customerService.updateMaster(customer);
-        }
-        List<Map<String, Object>> studios = domainQueryService.queryStudioBySite(page, city,typeName,name, latitude, longitude);
+        }*/
+        List<Map<String, Object>> studios = domainQueryService.queryStudioBySite(page, city, typeName,name, latitude, longitude);
         page.setRecords(studios);
 
         return SuccessTip.create(page);
